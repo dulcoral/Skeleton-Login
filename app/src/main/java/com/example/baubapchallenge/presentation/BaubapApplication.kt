a3ac3854b32c8dff8fa08fac9cb7fbb2f5836668
Coral Rodriguez Garcia <dulcoral20@gmail.com>
2022-09-26 17:30:55 -0500

    login ui

---
+++ app/src/main/java/com/example/baubapchallenge/presentation/BaubapApplication.kt
@@ -0,0 +1,26 @@
+package com.example.baubapchallenge.presentation
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaubapApplication : Application() {
+
+    companion object {
+        private lateinit var instance: BaubapApplication
+
+        fun getInstance(): BaubapApplication {
+            return instance
+        }
+
+        fun getAppContext(): Context {
+            return instance.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
--- app/src/main/java/com/example/baubapchallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/example/baubapchallenge/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.baubapchallenge.presentation.ui
+package com.example.baubapchallenge.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
